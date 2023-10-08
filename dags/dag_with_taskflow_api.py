41196990946af955865786b0ba325d9df7e98dfd
RArielAlonso <rariel.alonso@gmail.com>
2023-10-08 10:19:47 -0300

    modificacion en dag_with_taskflow_api

--- dags/dag_with_taskflow_api.py
+++ dags/dag_with_taskflow_api.py
@@ -1,25 +1,24 @@
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
+from airflow.decorators import dag, task
 
-from airflow.decorators import dag,task
 
-default_args={
-    'owner':'Ariel',
-    'retries':5,
-    'retry_delay':timedelta(minutes=5)
+default_args = {
+    'owner': 'Ariel',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
 }
+
 
 @dag(dag_id='dag_with_taskflow_api_v03',
      default_args=default_args,
-     start_date=datetime(2023,5,12),
+     start_date=datetime(2023, 10, 7),
      schedule_interval='@daily')
 def hello_world_elt():
-    
-
     @task(multiple_outputs=True)
     def get_name():
         return {
-            'first_name':'Ariel',
-            'last_name':'Alonso'
+            'first_name': 'Ariel',
+            'last_name': 'Alonso'
         }
     
     @task()
@@ -27,13 +26,14 @@ def hello_world_elt():
         return 36
     
     @task()
-    def greet(first_name,last_name,age):
+    def greet(first_name, last_name, age):
         print(f"Hello World ! My name is {first_name} {last_name} and i am {age} years old")
 
-    name_dict=get_name()
-    age=get_age()
+    name_dict = get_name()
+    age = get_age()
     greet(first_name=name_dict['first_name'],
           last_name=name_dict['last_name'],
           age=age)
 
-greet_dag=hello_world_elt()
+
+greet_dag = hello_world_elt()
